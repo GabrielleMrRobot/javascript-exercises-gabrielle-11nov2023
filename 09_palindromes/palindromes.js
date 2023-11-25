6773f3ce5600848980ae05aae481be88b97a3b32
GabrielleMrRobot <gbrizuelag@gmail.com>
2023-11-25 16:07:47 -0400

    feat: finish palindrome.js

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,19 @@
-const palindromes = function () {
+const palindromes = function (string) {
+    string = string.replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g,"");
+    string = string.toLowerCase();
+    result = false;
+    let count = 0;
+   for(let i=0; i<string.length; i++) {
+    if (string[i] === string[string.length -i - 1]) {
+        count++;
+    }
 
+   }
+   if(string.length === count){
+      result = true;
+
+   }
+   return result;
 };
 
 // Do not edit below this line
